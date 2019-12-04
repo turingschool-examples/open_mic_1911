fe2fdc1b39455e907baead1966fffa133ce5f9cd
Kathleen Carroll <khakicarroll@comcast.net>
2019-12-04 09:48:05 -0700

    Add test 4 for tell method

--- test/user_test.rb
+++ test/user_test.rb
@@ -6,6 +6,7 @@ require './lib/user'
 class UserTest < Minitest::Test
   def setup
     @sal = User.new("Sal")
+    @ali = User.new("Ali")
     @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
     @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
@@ -25,5 +26,16 @@ class UserTest < Minitest::Test
     assert_equal [@joke_1], @sal.jokes
     @sal.learn(@joke_2)
     assert_equal [@joke_1, @joke_2], @sal.jokes
+  end
+
+  def test_it_can_tell_jokes_it_learns
+    @sal.learn(@joke_1)
+    @sal.learn(@joke_2)
+
+    @sal.tell(@ali, @joke_1)
+    assert_equal [@joke_1], @ali.jokes
+    
+    @sal.tell(@ali, @joke_2)
+    assert_equal [@joke_1, @joke_2], @ali.jokes
   end
 end

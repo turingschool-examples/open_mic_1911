90a1cb38a73f85cc58e0ae9b163a2e17968923f5
Ryan Allen <rcallen89@gmail.com>
2019-12-04 09:54:11 -0700

    Create method to add performers to the open mic Create method to check for duplicate jokes from performers Create Test to verify method return values are followed

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,10 +1,27 @@
 #class file for Open Mic class
 
 class OpenMic
-  attr_reader :location, :date
+  attr_reader :location, :date, :performers
   def initialize(info)
     @location = info[:location]
     @date = info[:date]
+    @performers = []
+  end
+
+  def welcome(new_performer)
+    @performers.push(new_performer)
+  end
+
+  def repeated_jokes?
+    all_jokes = []
+    @performers.each do |user|
+      all_jokes.push(user.jokes)
+    end
+    if all_jokes.flatten.uniq! == nil
+      false
+    else
+      true
+    end
   end
 
 end
--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -7,6 +7,10 @@ require './lib/joke'
 class OpenMicTest < Minitest::Test
   def setup
     @open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    @sal = User.new("Sal")
+    @ali = User.new("Ali")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
 
   def test_it_exists
@@ -16,5 +20,27 @@ class OpenMicTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Comedy Works", @open_mic.location
     assert_equal "11-20-18", @open_mic.date
+    assert_equal [], @open_mic.performers
+  end
+
+  def test_that_performers_can_be_added
+    @open_mic.welcome(@sal)
+    @open_mic.welcome(@ali)
+
+    assert_equal @sal, @open_mic.performers[0]
+    assert_equal @ali, @open_mic.performers[1]
+  end
+
+  def test_that_there_are_repeated_jokes
+    @open_mic.welcome(@sal)
+    @open_mic.welcome(@ali)
+    @ali.learn(@joke_1)
+    @ali.learn(@joke_2)
+
+    assert_equal false, @open_mic.repeated_jokes?
+
+    @sal.learn(@joke_1)
+
+    assert_equal true, @open_mic.repeated_jokes?
   end
 end

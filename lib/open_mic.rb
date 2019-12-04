27722e6a0040760abde4fef4e51bf4838e586fad
Daniel Selph <daniel.p.selph@gmail.com>
2019-12-04 10:59:16 -0700

    Working on repeated_jokes? method for iteration 4

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,11 +1,22 @@
 class OpenMic
+  attr_reader :location, :date, :performers
 
   def initialize(location:, date:)
     @location = location
     @date = date
+    @performers = []
   end
 
+  def welcome(user)
+    @performers << user
+  end
 
+  def repeated_jokes?
+    # return true if @user.jokes.length > 2
+    # false
+
+    #false
+  end
 
 
 

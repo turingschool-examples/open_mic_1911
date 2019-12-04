d86532b8d642ba46c26d262b292405c2fdcbb9e5
Ezekiel Clark <clarkzeke@gmail.com>
2019-12-04 09:18:40 -0700

    Add user exist test

--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,12 @@
 class User
 
+  attr_reader :name, :jokes
   def initialize(name)
     @name = name
     @jokes = []
   end
 
+  def learn(joke)
+    @jokes << joke  
+  end
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -2,3 +2,17 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/joke'
 require './lib/user'
+
+class UserTest < Minitest::Test
+
+
+  def test_user_exists
+    sal = User.new("Sal")
+
+    assert_instance_of User, sal
+  end
+
+  def test_attributes
+
+  end
+end

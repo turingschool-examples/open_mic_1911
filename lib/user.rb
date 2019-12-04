345a4c80b15aa043ec485ade28af34ef3821df0a
Fred Rondina <fredrondina96@gmail.com>
2019-12-04 10:07:31 -0700

    Fixed formatting in user joke_by_id method

--- lib/user.rb
+++ lib/user.rb
@@ -19,12 +19,11 @@ attr_reader :name, :jokes
   end
 
   def joke_by_id(number)
-    # binding.pry
     # self.select {|number| self.jokes.id}
-    self.jokes.each do |search|
-      if search.id == number
-        return search
+      self.jokes.each do |search|
+        if search.id == number
+          return search
+        end
       end
-    end
   end
 end

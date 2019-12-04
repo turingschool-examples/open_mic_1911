92715d5151286e23f885083c730b914165db3308
Kathleen Carroll <khakicarroll@comcast.net>
2019-12-04 10:33:07 -0700

    Add test 3 to evaluate the welcome method

--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -7,6 +7,8 @@ require './lib/open_mic'
 class UserTest < Minitest::Test
   def setup
     @open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    @sal = User.new("Sal")
+    @ali = User.new("Ali")
   end
 
   def test_it_exists
@@ -17,5 +19,13 @@ class UserTest < Minitest::Test
     assert_equal "Comedy Works", @open_mic.location
     assert_equal "11-20-18", @open_mic.date
     assert_equal [], @open_mic.performers
+  end
+
+  def test_performers_are_welcomed
+    open_mic.welcome(@sal)
+    assert_equal [@sal], @open_mic.performers
+
+    open_mic.welcome(@ali)
+    assert_equal [@sal, @ali], @open_mic.performers
   end
 end

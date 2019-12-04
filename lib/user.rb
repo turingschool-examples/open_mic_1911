317764b24297c176842c38b8f8f677e7a9a9c930
Jordan <jordan.wallace.williams@gmail.com>
2019-12-04 09:41:14 -0700

    Add and update logic and tests for user class methods

--- lib/user.rb
+++ lib/user.rb
@@ -10,4 +10,14 @@ class User
   def learn(joke)
     @jokes << joke
   end
+
+  def tell(user, joke)
+    user.jokes << joke
+  end
+
+  def joke_by_id(id)
+    @jokes.find do |joke|
+      return joke if joke.id == id
+    end
+  end
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -29,4 +29,32 @@ class UserTest < Minitest::Test
     assert_equal 1, sal.jokes[0].id
     assert_equal 2, sal.jokes[1].id
   end
+
+  def test_multiple_users_can_tell_jokes_to_other_users
+    sal = User.new('Sal')
+    ali = User.new("Ali")
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    sal.learn(joke_1)
+    sal.learn(joke_2)
+    sal.tell(ali, joke_1)
+    sal.tell(ali, joke_2)
+
+    assert_equal 1, ali.jokes[0].id
+    assert_equal 2, ali.jokes[1].id
+  end
+
+  def test_can_find_users_jokes_by_id
+    sal = User.new('Sal')
+    ali = User.new("Ali")
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    sal.learn(joke_1)
+    sal.learn(joke_2)
+    sal.tell(ali, joke_1)
+    sal.tell(ali, joke_2)
+
+    assert_equal joke_1, ali.joke_by_id(1)
+    assert_equal joke_2, ali.joke_by_id(2)
+  end
 end

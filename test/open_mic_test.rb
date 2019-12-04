6a366a4f0cfdfb136f4e115ce90521172a892d84
Kathleen Carroll <khakicarroll@comcast.net>
2019-12-04 10:42:50 -0700

    Add 4th test to evaluate the repeated_jokes? method

--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -9,6 +9,8 @@ class UserTest < Minitest::Test
     @open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
     @sal = User.new("Sal")
     @ali = User.new("Ali")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
 
   def test_it_exists
@@ -27,5 +29,14 @@ class UserTest < Minitest::Test
 
     @open_mic.welcome(@ali)
     assert_equal [@sal, @ali], @open_mic.performers
+  end
+
+  def test_performers_learn_unique_jokes
+    @ali.learn(@joke_1)
+    @ali.learn(@joke_2)
+    assert_equal false, @open_mic.repeated_jokes?
+
+    @sal.learn(@joke_1)
+    assert_equal true, @open_mic.repeated_jokes?
   end
 end

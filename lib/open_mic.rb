bcd67ad8f0a61ac626e1b15f8bc9a1064d7912be
Nathan Keller <keller.nathan@gmail.com>
2019-12-04 09:52:08 -0700

    Open_mic class created. Working on repeated_jokes? method

---
+++ lib/open_mic.rb
@@ -0,0 +1,47 @@
+class OpenMic
+  attr_reader :char_params, :performers
+
+  def initialize(char_params)
+    @location = char_params[:location]
+    @date = char_params[:date]
+    @performers = []
+  end
+
+  def location
+    @location
+  end
+
+  def date
+    @date
+  end
+
+  def welcome(performer)
+    @performers << performer
+  end
+
+  def repeated_jokes
+    # return true if 
+
+  end
+
+end
+
+
+# pry(main)> joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+# # => #<Joke:0x00007fe8fd892978...>
+#
+# pry(main)> joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+# # => #<Joke:0x00007fe8fe19f250...>
+#
+# pry(main)> ali.learn(joke_1)
+#
+# pry(main)> ali.learn(joke_2)
+#
+# pry(main)> open_mic.repeated_jokes?
+# # => false
+#
+# pry(main)> sal.learn(joke_1)
+#
+# pry(main)> open_mic.repeated_jokes?
+# # => true
+# ```
---
+++ test/open_mic_test.rb
@@ -0,0 +1,43 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/open_mic'
+require './lib/user'
+require './lib/joke'
+
+class OpenMicTest < Minitest::Test
+
+  def setup
+    @open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    @user_1 = User.new("Sal")
+    @user_2 = User.new("Ali")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+  end
+
+  def test_it_exists
+    assert_instance_of OpenMic, @open_mic
+  end
+
+  def test_attributes
+    assert_equal "Comedy Works", @open_mic.location
+    assert_equal "11-20-18", @open_mic.date
+    assert_equal [], @open_mic.performers
+  end
+
+  def test_welcome_performers
+    @open_mic.welcome(@user_1)
+    @open_mic.welcome(@user_2)
+    assert_equal [@user_1, @user_2], @open_mic.performers
+  end
+
+  def test_check_for_repeated_jokes
+    @open_mic.welcome(@user_1)
+    @open_mic.welcome(@user_2)
+    @user_2.learn(@joke_1)
+    @user_2.learn(@joke_2)
+    @user_1.learn(@joke_1)
+    require "pry"; binding.pry
+    assert_equal false, @open_mic.repeated_jokes?
+    assert_equal true, @open_mic.repeated_jokes?
+  end
+end

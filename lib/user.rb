d71962e85983d87cdd27a9e5aa74aaeee5a1f541
Stephanie Friend <stephaniefriend91@gmail.com>
2019-12-04 10:30:28 -0700

    Working on the joke_by_id method

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,11 @@
 class User
 
-  attr_reader :name, :jokes
+  attr_reader :name, :jokes, :joke
 
   def initialize(name)
     @name = name
     @jokes = []
+    @joke = joke
 
   end
 
@@ -15,4 +16,11 @@ class User
   def tell(name, joke)
     name.learn(joke)
   end
+  # I have tried to work this one out in several
+  # ways, but I cannot figure it out, so I am moving on.
+  # def joke_by_id(new_id)
+  #   if @joke.id == new_id
+  #     return joke
+  #   end
+  # end
 end

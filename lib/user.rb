3cf0d15fb808b5f35e3e0f9f36c13e331e0c8337
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-12-04 09:20:00 -0700

    Add learn method and passing tests

--- lib/user.rb
+++ lib/user.rb
@@ -2,10 +2,15 @@
 #     User has a name and uses an array to store jokes
 
 class User
-  attr_reader :name
-  
+  attr_reader :name, :jokes
+
   def initialize(name)
     @name = name
+    @jokes = []
+  end
+
+  def learn(joke)
+    @jokes.push(joke)
   end
 
 end

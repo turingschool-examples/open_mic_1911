2c174bf074ff4e5a998203611d07e64255636b91
William Meighan <williamdmeighan@gmail.com>
2019-12-04 09:54:16 -0700

    Add question for instructors regarding repated_jokes? method

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -17,6 +17,8 @@ class OpenMic
       user.jokes
     end.flatten
 
+
+    # Wondering if there might be a cleaner way to test for repeating jokes. I tried to look up some possible enumerables but wasn't having much luck, although I'm sure there is a way. Let me know what you think!
     if jokes_array.uniq.length < jokes_array.length
       true
     else

58f169464fe65a0b6a45f1e1eb42b2b59ec9497b
Kathleen Carroll <khakicarroll@comcast.net>
2019-12-04 09:28:08 -0700

    Add test 2

--- test/user_test.rb
+++ test/user_test.rb
@@ -4,8 +4,16 @@ require './lib/joke'
 require './lib/user'
 
 class UserTest < Minitest::Test
+  def setup
+    @sal = User.new("Sal")
+  end
+
   def test_it_exists
-    sal = User.new("Sal")
-    assert_instance_of User, sal
+    assert_instance_of User, @sal
+  end
+
+  def test_it_has_attributes
+    assert_equal "Sal", @sal.name
+    assert_equal [], @sal.jokes
   end
 end

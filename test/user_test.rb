28ece97d1ac50d92596ddb3792bd37d2e0d3f5ac
Jordan <jordan.wallace.williams@gmail.com>
2019-12-04 09:24:42 -0700

    Add tests for user class

--- test/user_test.rb
+++ test/user_test.rb
@@ -11,4 +11,22 @@ class UserTest < Minitest::Test
 
     assert_instance_of User, sal
   end
+
+  def test_user_starts_with_name_and_no_jokes
+    sal = User.new('Sal')
+
+    assert_equal 'Sal', sal.name
+    assert_equal [], sal.jokes
+  end
+
+  def test_jokes_can_be_added_to_user
+    sal = User.new('Sal')
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    sal.learn(joke_1)
+    sal.learn(joke_2)
+
+    assert_equal 1, sal.jokes[0].id
+    assert_equal 2, sal.jokes[1].id
+  end
 end

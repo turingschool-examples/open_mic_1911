13f0eaa5e2cf0f3f0ca6ded6ede293a4a44ce113
Jordan <jordan.wallace.williams@gmail.com>
2019-12-04 10:23:33 -0700

    Add repeated jokes method

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,3 +1,28 @@
 class OpenMic
+  attr_reader :location, :date
+  attr_accessor :performers
 
+  def initialize(data)
+    @location = data[:location]
+    @date = data[:date]
+    @performers = []
+  end
+
+  def welcome(performer)
+    @performers << performer
+  end
+
+  def repeated_jokes?
+    the_jokes = []
+    @performers.each do |performer|
+      performer.jokes.each do |joke|
+        the_jokes << joke.id
+      end
+    end
+    if the_jokes.uniq.length == the_jokes.length
+      false
+    else
+      true
+    end
+  end
 end

72d261bbc0d2c92224b32e49825f20308e98a6ba
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-12-04 09:44:34 -0700

    Pass test for attributes

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,7 +1,10 @@
 class OpenMic
+  attr_reader :location, :date, :performers
+
   def initialize(open_mic_info)
     @location = open_mic_info[:location]
     @date = open_mic_info[:date]
     @performers = []
   end
+  
 end

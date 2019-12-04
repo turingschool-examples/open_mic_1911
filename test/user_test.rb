d7313182b4c63c1951c1e17f8e0c85d4b7468152
David Atkins <atki1080@gmail.com>
2019-12-04 09:26:23 -0700

    Test joke_by_id

--- test/user_test.rb
+++ test/user_test.rb
@@ -8,7 +8,7 @@ class UserTest < Minitest::Test
     @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
     @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
-  
+
   def test_it_exists
     sal = User.new("Sal")
     assert_instance_of User, sal
@@ -34,6 +34,18 @@ class UserTest < Minitest::Test
     sal.tell(ali, @joke_1)
     sal.tell(ali, @joke_2)
     assert_equal [@joke_1, @joke_2], ali.jokes
+  end
+
+  def test_joke_by_id
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    sal.learn(@joke_1)
+    sal.learn(@joke_2)
+    sal.tell(ali, @joke_1)
+    sal.tell(ali, @joke_2)
+    assert_equal @joke_1, ali.joke_by_id(1)
+    assert_equal @joke_2, ali.joke_by_id(2)
+    assert_nil ali.joke_by_id(3)
   end
 
 end

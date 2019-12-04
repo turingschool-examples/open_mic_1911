0be0758804176373aa4b4123dec4a3cb53eedafa
Tyler Tomlinson <twtomlinson89@gmail.com>
2019-12-04 11:01:31 -0700

    created and running test that have been made from open_mic_test

---
+++ lib/open_mic.rb
@@ -0,0 +1,15 @@
+require 'pry'
+class OpenMic
+
+attr_reader :name, :date, :welcome
+
+  def initialize (name)
+  @name = name
+  end
+
+end
+
+#date method
+#performers method
+#welcome method
+#repeated joke method

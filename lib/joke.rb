693774837688da9a6461d6a4350d70455d5d1394
Fred Rondina <fredrondina96@gmail.com>
2019-12-04 09:20:05 -0700

    Added joke class functionality

---
+++ lib/joke.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class Joke
+
+attr_reader :id, :setup, :punchline
+
+  def initialize(id, setup, punchline)
+    @id = id
+    @setup = setup
+    @punchline = punchline
+  end
+
+end

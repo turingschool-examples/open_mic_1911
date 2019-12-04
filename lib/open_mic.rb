9f8ebe6d949532c1bf19a603129467a26b199c92
David Atkins <atki1080@gmail.com>
2019-12-04 09:36:00 -0700

    Add initialize and welcome methods

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,5 +1,13 @@
 class OpenMic
+  attr_reader :location, :date, :performers
   def initialize(info)
     @info = info
+    @location = info[:location]
+    @date = info[:date]
+    @performers = []
+  end
+
+  def welcome(performer)
+    @performers << performer
   end
 end

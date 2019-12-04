15225dd6e5e72cb4b9dc69c57aebff9b9d2291eb
Ana Perez Santos <a.jperez@yahoo.com>
2019-12-04 09:29:31 -0700

    OpenMic It. 2: test user can learn jokes, include learn method, pass test

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,13 @@
 class User
-
   attr_reader :jokes
 
   def initialize(name)
     @name = name
     @jokes = []
+  end
+
+  def learn(joke_name)
+    @jokes << joke_name
   end
 
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -17,4 +17,15 @@ class UserTest < Minitest::Test
     assert_equal [], sal.jokes
   end
 
+  def test_user_can_learn_jokes
+    sal = User.new("Sal")
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+
+    sal.learn(joke_1)
+    sal.learn(joke_2)
+
+    assert_equal [joke_1, joke_2], sal.jokes
+  end
+
 end

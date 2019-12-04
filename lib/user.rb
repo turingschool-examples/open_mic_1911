e54d2d4174f376b8e7549f8589114d8e9c78987a
Nathan Keller <keller.nathan@gmail.com>
2019-12-04 09:14:44 -0700

    Iteration 2 complete

--- lib/user.rb
+++ lib/user.rb
@@ -6,21 +6,8 @@ class User
     @jokes = []
   end
 
-end
+  def learn_joke(joke)
+    @jokes << joke
+  end
 
-# pry(main)> sal.jokes
-# # => []
-#
-# pry(main)> joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
-# # => #<Joke:0x00007fb71da169f0...>
-#
-# pry(main)> joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
-# # => #<Joke:0x00007fb71d8e0bd0...>
-#
-# pry(main)> sal.learn(joke_1)
-#
-# pry(main)> sal.learn(joke_2)
-#
-# pry(main)> sal.jokes
-# # => [#<Joke:0x00007fb71da169f0...>, #<Joke:0x00007fb71d8e0bd0...>]
-# ```
+end
--- test/user_test.rb
+++ test/user_test.rb
@@ -5,7 +5,10 @@ require './lib/user'
 
 class UserTest < Minitest::Test
   def setup
-    @user = User.new("Sal")
+    @user_1 = User.new("Sal")
+    @user_2 = User.new("Ali")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
 
   def test_it_exists
@@ -18,6 +21,12 @@ class UserTest < Minitest::Test
 
   def test_it_can_hold_jokes
     assert_equal [], @user.jokes
+  end
+
+  def test_it_can_learn_jokes
+    @user.learn_joke(@joke_1)
+    @user.learn_joke(@joke_2)
+    assert_equal [@joke_1, @joke_2], @user.jokes
   end
 
 end

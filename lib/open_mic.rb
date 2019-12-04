b8568f9fc4aae7ee0e7462b1caaf06e56ded2be3
Jordan Sewell <jrsewell400@gmail.com>
2019-12-04 10:30:52 -0700

    Add Iteration 4 files and first test

---
+++ lib/open_mic.rb
@@ -0,0 +1,10 @@
+class OpenMic
+
+  attr_reader :location, :date
+
+  def initialize(location: "Comedy Works", date: "11-20-18")
+    @location = location
+    @date = date
+  end
+
+end
---
+++ test/open_mic_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/open_mic'
+require './lib/user'
+require './lib/joke'
+
+class OpenMicTest < Minitest::Test
+  def setup
+    @open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+  end
+
+  def test_it_exists
+    assert OpenMic, @open_mic
+  end
+end

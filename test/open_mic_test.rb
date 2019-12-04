6128ac7d699f4d9f2f8f5662611df2ba2d183547
David Atkins <atki1080@gmail.com>
2019-12-04 09:36:14 -0700

    Test attributes and welcome

--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -5,8 +5,25 @@ require './lib/user'
 require './lib/joke'
 
 class OpenMicTest < Minitest::Test
+  def setup
+    @open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    @sal = User.new("Sal")
+    @ali = User.new("Ali")
+  end
+
   def test_it_exists
-    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
-    assert_instance_of OpenMic, open_mic
+    assert_instance_of OpenMic, @open_mic
+  end
+
+  def test_it_has_attributes
+    assert_equal "Comedy Works", @open_mic.location
+    assert_equal "11-20-18", @open_mic.date
+    assert_equal [], @open_mic.performers
+  end
+
+  def test_it_can_welcome_performers
+    @open_mic.welcome(@sal)
+    @open_mic.welcome(@ali)
+    assert_equal [@sal, @ali], @open_mic.performers
   end
 end

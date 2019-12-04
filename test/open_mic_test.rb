6d17cfae1af674b09fff83fbc4771009b6b55f93
Kathleen Carroll <khakicarroll@comcast.net>
2019-12-04 10:27:18 -0700

    Add second test to evaluate openmic attributes

--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -12,4 +12,9 @@ class UserTest < Minitest::Test
   def test_it_exists
     assert_instance_of OpenMic, @open_mic
   end
+
+  def test_it_has_attributes
+    assert_equal "Comedy Works", @open_mic.location
+    assert_equal "11-20-18", @open_mic.date
+  end
 end

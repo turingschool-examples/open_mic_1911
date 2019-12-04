a9c262f61be3fb091cfac08a472ba09cc62ee3c9
William Meighan <williamdmeighan@gmail.com>
2019-12-04 09:24:11 -0700

    Add passing user class for iteration 3

--- lib/user.rb
+++ lib/user.rb
@@ -8,7 +8,17 @@ class User
   end
 
   def learn(joke)
-    @jokes << joke  
+    @jokes << joke
+  end
+
+  def tell(user, joke)
+    user.learn(joke)
+  end
+
+  def joke_by_id(id)
+    @jokes.find do |joke|
+      joke.id == id
+    end
   end
 
 end

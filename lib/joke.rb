01e51c4bb7c9da2136fc306ff47f39554accc22c
Jenny Klich <jennyklich@gmail.com>
2019-12-04 09:53:48 -0700

    Made change to joke file

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,7 +1,8 @@
 class Joke
-attr_reader :setup, :punchline
+attr_reader :joke, :setup, :punchline
 
   def initialize(joke, setup, punchline)
+    @joke = joke
     @setup = setup
     @punchline = punchline
   end

46ba59ba94d31138c7196b8fe82cf41a3191627c
David Holtkamp <david.hholtkamp@gmail.com>
2019-12-04 09:42:49 -0700

    Methods for learning and telling and unique id

--- lib/user.rb
+++ lib/user.rb
@@ -8,6 +8,16 @@ attr_reader :name,
   end
 
   def learn(joke)
-    jokes << joke 
+    jokes << joke
+  end
+
+  def tell(recipient, joke)
+    recipient.jokes << joke
+  end
+
+  def joke_by_id(id)
+    @jokes.find do |joke|
+      joke.id == id
+    end 
   end
 end

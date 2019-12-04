69927a6b43e2e45b179e9aa7bf05da7c399a10b2
Ezekiel Clark <clarkzeke@gmail.com>
2019-12-04 09:20:53 -0700

    Add setup

--- test/user_test.rb
+++ test/user_test.rb
@@ -5,11 +5,14 @@ require './lib/user'
 
 class UserTest < Minitest::Test
 
+  def setup
+    @sal = User.new("Sal")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+  end
 
   def test_user_exists
-    sal = User.new("Sal")
-
-    assert_instance_of User, sal
+    assert_instance_of User, @sal
   end
 
   def test_attributes

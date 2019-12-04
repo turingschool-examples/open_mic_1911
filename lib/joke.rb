e6176365fdf722f62ddc5a5e8e7a823a45e72763
Alex Lynch <mlynch5187@gmail.com>
2019-12-04 09:31:35 -0700

    Update joke.rb

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,18 +1,3 @@
-# pry(main)> require './lib/joke'
-# # => true
-#
-# pry(main)> joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
-# # => #<Joke:0x00007f84602e4190...>
-#
-# pry(main)> joke.id
-# # => 1
-#
-# pry(main)> joke.setup
-# # => "Why did the strawberry cross the road?"
-#
-# pry(main)> joke.punchline
-# # => "Because his mother was in a jam."
-# ```
 class Joke
 
   attr_reader :joke, :number, :answer

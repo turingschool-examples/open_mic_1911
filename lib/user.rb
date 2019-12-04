a54405f722fedf5be140d97542addc4089d79eb9
Kelly Bard <kellyibard@gmail.com>
2019-12-04 09:24:01 -0700

    Added joke and user class. Added user test file.

---
+++ lib/user.rb
@@ -0,0 +1,31 @@
+class User
+  attr_reader :name, :jokes
+
+  def initialize(name)
+    @name = name
+    @jokes = []
+  end
+
+  def learn(joke)
+    @jokes << joke
+  end
+
+  def tell(user, joke)
+    user.learn(joke)
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end

b25bd438c516e0f533b10fdaf3570db6cbd5f1a0
William Meighan <williamdmeighan@gmail.com>
2019-12-04 09:19:31 -0700

    Add test_it_can_tell_jokes and test_it_can_locate_joke_by_id as specified by the iteration 3 interaction pattern

--- test/user_test.rb
+++ test/user_test.rb
@@ -7,6 +7,7 @@ class UserTest < Minitest::Test
 
   def setup
     @sal = User.new("Sal")
+    @ali = User.new("Ali")
 
     @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
     @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
@@ -26,6 +27,27 @@ class UserTest < Minitest::Test
     @sal.learn(@joke_2)
 
     assert_equal [@joke_1, @joke_2], @sal.jokes
+  end
+
+  def test_it_can_tell_jokes
+    @sal.learn(@joke_1)
+    @sal.learn(@joke_2)
+
+    @sal.tell(@ali, @joke_1)
+    @sal.tell(@ali, @joke_2)
+
+    assert_equal [@joke_1, @joke_2], @ali.jokes
+  end
+
+  def test_it_can_locate_joke_by_id
+    @sal.learn(@joke_1)
+    @sal.learn(@joke_2)
+
+    @sal.tell(@ali, @joke_1)
+    @sal.tell(@ali, @joke_2)
+
+    assert_equal @joke_1, @ali.joke_by_id(1)
+    assert_equal @joke_2, @ali.joke_by_id(2)
   end
 
 end

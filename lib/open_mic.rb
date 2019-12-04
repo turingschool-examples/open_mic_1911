08c27d61fb76615d7ab5742fa5f6937e4bda5bf9
Kathleen Carroll <khakicarroll@comcast.net>
2019-12-04 10:30:36 -0700

    Update class to include performers method

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,8 +1,9 @@
 class OpenMic
-  attr_reader :location, :date
+  attr_reader :location, :date, :performers
 
   def initialize(attributes)
     @location = attributes[:location]
     @date = attributes[:date]
+    @performers = []
   end
 end

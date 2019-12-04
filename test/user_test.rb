c3ab14fe7e0b98b383f7cfafd987fb256d63e674
Daniel Selph <daniel.p.selph@gmail.com>
2019-12-04 09:15:10 -0700

    User test and class created, working on learn jokes method

---
+++ test/user_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/joke'
+require './lib/user'
+
+class UserTest < Minitest::Test
+
+  def setup
+    @sal = User.new("Sal")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+  end
+
+  def test_user_exists
+    assert_instance_of User, @sal
+  end
+
+  def test_user_has_empty_jokes_array
+    assert_equal [], @sal.jokes
+  end
+
+
+
+end

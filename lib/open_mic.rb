8d7ece81f3b70368df3538e09ecb8826effb79a6
Chase.Gaddis <cgaddis36@gmail.com>
2019-12-04 10:43:09 -0700

    updates to openmic for itration 4

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,10 +1,9 @@
 class OpenMic
   attr_reader :location, :date
-  def initialize(details)
-    @details = {:location, :date => date}
+  def initialize(location:, date:)
+    @location = location
+    @date = date
     @performers = []
   end
-
-
 
 end

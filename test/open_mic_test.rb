6f07d7f55b26a9ecc47fdbf5e3da234db539cc12
Jordan <jordan.wallace.williams@gmail.com>
2019-12-04 09:41:55 -0700

    Add open mic test file

---
+++ test/open_mic_test.rb
@@ -0,0 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/joke'
+require './lib/user'
+require './lib/open_mic'
+
+class OpenMicTest < Minitest::Test
+
+end

808338f6b5d9e445a31715e37d355aa1e523964d
Kevin McGrevey <niv3kmcg@gmail.com>
2019-12-04 10:56:51 -0700

    Final commit for timed test limit

--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,5 @@
 class User
-  attr_reader :name, :jokes
+  attr_reader :name, :jokes, :id
 
   def initialize(name)
     @name = name
@@ -8,6 +8,22 @@ class User
 
   def learn(new_joke)
     @jokes << new_joke
+  end
+
+  def tell(audience, joke)
+    audience.jokes << joke
+  end
+
+  def joke_by_id(index)
+    #>>>need interate over array to return joke associated with @id from Joke<<<
+    # require "pry"; binding.pry
+    # @jokes.id
+    # jokes[index - 1]
+    if jokes[index - 1].id == index
+      jokes[index - 1]
+    end
+
+    #need interate over array to return joke associated with @id from Joke
   end
 
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -29,7 +29,38 @@ class UserTest < Minitest::Test
     assert_equal [joke_1, joke_2], sal.jokes
   end
 
+  def test_user_can_tell_joke_to_another_user
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
 
+    sal.learn(joke_1)
+    sal.learn(joke_2)
+    sal.tell(ali, joke_1)
+    assert_equal [joke_1], ali.jokes
+    sal.tell(ali, joke_2)
+    assert_equal [joke_1, joke_2], ali.jokes
+  end
 
+  def test_can_sort_jokes_by_index
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    joke_7 = Joke.new(7, "Knock knock", "Who's there?")
+
+    sal.learn(joke_1)
+    sal.learn(joke_2)
+    sal.learn(joke_7)
+
+    sal.tell(ali, joke_1)
+    sal.tell(ali, joke_2)
+    sal.tell(ali, joke_7)
+
+    assert_equal joke_1, ali.joke_by_id(1)
+    assert_equal joke_2, ali.joke_by_id(2)
+    assert_equal joke_7, ali.joke_by_id(7)
+  end
 
 end

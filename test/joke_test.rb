1edb062e8a07752f06182245af813084d224d0a1
Stephanie Friend <stephaniefriend91@gmail.com>
2019-12-04 11:00:20 -0700

    Started on the open_mic iteration 4

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/joke'
 require './lib/user'
+require './lib/open_mic'
 
 class JokeTest < Minitest::Test
   def test_it_exists
@@ -74,4 +75,58 @@ class JokeTest < Minitest::Test
   #   assert_equal joke_1, ali.joke_by_id(1)
   #   assert_equal joke_2, ali.joke_by_id(2)
   # end
+
+  def test_open_mic_has_attributes
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+
+    assert_equal "Comedy Works", open_mic.location
+    assert_equal "11-20-18", open_mic.date
+  end
+
+  def test_when_open_mic_stats_it_has_no_performers
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+
+    assert_equal [], open_mic.performers
+  end
+
+  def test_open_mic_welcomes_users
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+
+    open_mic.welcome(sal)
+    open_mic.welcome(ali)
+
+    assert_equal [sal, ali], open_mic.performers
+  end
+
+  def test_repeated_jokes_default_at_false
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+
+    ali.learn(joke_1)
+    ali.learn(joke_2)
+
+    refute open_mic.repeated_jokes?
+  end
+
+  def test_when_a_joke_is_told_twice_repeated_joke_returns_true
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+
+    ali.learn(joke_1)
+    ali.learn(joke_2)
+
+    refute open_mic.repeated_jokes?
+
+    sal.learn(joke_1)
+
+    assert open_mic.repeated_jokes?
+  end 
 end

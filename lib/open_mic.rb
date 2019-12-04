64252b3a1642845ad5b14be20805a288914eabee
Elom Amouzou <elom1990@yahoo.com>
2019-12-04 10:31:50 -0700

    Adjusted welcome method for Iteration 4

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,16 +1,19 @@
 class OpenMic
-  attr_reader :attr, :location, :date, :performers
+  attr_reader :attr, :location, :date, :performers, :comp
 
   def initialize(attr = {})
     @attr = attr
     @location = attr[:location]
     @date = attr[:date]
     @performers = []
+    @comp = {}
     # require "pry"; binding.pry
   end
 
   def welcome(user)
     @performers << user
+    @comp["#{user.name} jokes"] = [] if comp[user.name].nil?
+    @comp["#{user.name} jokes"] << user.jokes
   end
 
   def repeated_jokes?

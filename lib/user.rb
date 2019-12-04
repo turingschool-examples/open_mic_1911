dd9d79aae7fc038c273c82e4360d7016207e483d
Ana Perez Santos <a.jperez@yahoo.com>
2019-12-04 10:34:44 -0700

    OpenMic It. 3: uncomment lookup by id test, include guiding comments, iterate to find mathing id

--- lib/user.rb
+++ lib/user.rb
@@ -14,5 +14,13 @@ class User
     user.learn(joke_name)
   end
 
+  def joke_by_id(id_number)
+    # look through jokes
+    @jokes.find do |joke|
+      joke.id == id_number
+    end
+    # find joke whose id matches the id_number given
+    # return that joke
+  end
 
 end

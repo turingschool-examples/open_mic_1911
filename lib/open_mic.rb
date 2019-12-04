cf5a457c8be7bd9176aabbf3e5c25d4654bb97f6
Ana Perez Santos <a.jperez@yahoo.com>
2019-12-04 10:46:44 -0700

    OpenMic Iteration 4: create test/lib files for open mic, initialize

---
+++ lib/open_mic.rb
@@ -0,0 +1,6 @@
+class OpenMic
+
+  def initialize(info)
+
+  end
+end
---
+++ test/open_mic_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/open_mic'
+require './lib/user'
+require './lib/joke'
+
+class OpenMicTest < Minitest::Test
+  def test_it_exists
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+
+    assert_instance_of OpenMic, open_mic
+  end
+
+  
+end

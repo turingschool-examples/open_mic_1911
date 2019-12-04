345cc9737e53c5bb8379ea7f613703facf4da08a
Ana Perez Santos <a.jperez@yahoo.com>
2019-12-04 09:24:33 -0700

    OpenMic It. 2: test user starts with no jokes, include jokes attribute

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,10 @@
 class User
 
+  attr_reader :jokes
+
   def initialize(name)
     @name = name
+    @jokes = []
   end
 
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -4,11 +4,17 @@ require './lib/user'
 require './lib/joke'
 
 class UserTest < Minitest::Test
-  
+
   def test_it_exists
     sal = User.new("Sal")
 
     assert_instance_of User, sal
+  end
+
+  def test_user_starts_with_no_jokes
+    sal = User.new("Sal")
+
+    assert_equal [], sal.jokes
   end
 
 end

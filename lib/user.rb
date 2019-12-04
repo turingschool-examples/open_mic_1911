25af6ae63bbcd3e0965d17d4b184fa8406883612
Jordan <jordan.wallace.williams@gmail.com>
2019-12-04 09:24:11 -0700

    Add logic and methods to user class

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,13 @@
 class User
-  attr_reader :name, :jokes
+  attr_reader :name
+  attr_accessor :jokes
 
   def initialize(name)
     @name = name
+    @jokes = []
+  end
+
+  def learn(joke)
+    @jokes << joke
   end
 end

d6c7f7852e3f5cb7d9b9565e446a628c49ae4dc2
Timo Tyrrell <timomitchel23@gmail.com>
2019-12-05 09:46:37 -0700

    Finish user testing for iteration 2

--- test/user_test.rb
+++ test/user_test.rb
@@ -1,23 +1,9 @@
-
-# pry(main)> joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
-# # => #<Joke:0x00007fb71da169f0...>
-#
-# pry(main)> joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
-# # => #<Joke:0x00007fb71d8e0bd0...>
-#
 # pry(main)> sal.learn(joke_1)
 #
 # pry(main)> sal.learn(joke_2)
 #
 # pry(main)> sal.jokes
 # # => [#<Joke:0x00007fb71da169f0...>, #<Joke:0x00007fb71d8e0bd0...>]
-# ```
-
-# pry(main)> sal.name
-# # => "Sal"
-#
-# pry(main)> sal.jokes
-# # => []
 #
 require 'minitest/autorun'
 require 'minitest/pride'
@@ -26,16 +12,25 @@ require './lib/user'
 
 class UserTest < Minitest::Test
 
-  def test_it_exists
-    sal = User.new("Sal")
+  def setup
+    @sal = User.new("Sal")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+  end
 
-    assert_instance_of User, sal
+  def test_it_exists
+    assert_instance_of User, @sal
   end
 
   def test_user_attributes_are_accessible
-    sal = User.new("Sal")
+    assert_equal "Sal", @sal.name
+    assert_equal [], @sal.jokes
+  end
 
-    assert_equal "Sal", sal.name
-    assert_equal [], sal.jokes
+  def test_user_can_learn_jokes
+    @sal.learn(@joke_2)
+    @sal.learn(@joke_1)
+
+    assert_equal [@joke_2, @joke_1], @sal.jokes
   end
 end

5fd89d238e2e3220997b2929d0a3fb63629bc58e
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-12-05 11:06:44 -0700

    Adds User#joke_by_id

--- lib/user.rb
+++ lib/user.rb
@@ -9,4 +9,24 @@ class User
   def learn(joke)
     @jokes << joke
   end
+
+  def tell(user, told_joke)
+    user.learn(told_joke)
+    # user.jokes << joke
+  end
+
+  def joke_by_id(id)
+    found_joke = nil
+    @jokes.each do |joke|
+      if joke.id == id
+        #send that joke as the return value of this method
+        found_joke = joke
+      end
+    end
+    found_joke
+
+    # @jokes.find do |joke|
+    #   joke.id == id
+    # end
+  end
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -39,4 +39,32 @@ class UserTest < Minitest::Test
     # assert_equal true, sal.jokes.include?(joke_1)
     # assert_equal true, sal.jokes.include?(joke_2)
   end
+
+  def test_it_can_tell_jokes
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    megan = User.new("Megan")
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    sal.learn(joke_1)
+    sal.learn(joke_2)
+
+    sal.tell(ali, joke_1)
+    sal.tell(ali, joke_2)
+    ali.tell(megan, joke_1)
+
+    assert_equal [joke_1, joke_2], ali.jokes
+    assert_equal [joke_1], megan.jokes
+  end
+
+  def test_it_can_find_a_joke_by_id
+    sal = User.new("Sal")
+    joke_1 = Joke.new(1343234, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(222, "How do you keep a lion from charging?", "Take away its credit cards.")
+    sal.learn(joke_1)
+    sal.learn(joke_2)
+
+    assert_equal joke_2, sal.joke_by_id(222)
+    assert_nil sal.joke_by_id(54)
+  end
 end

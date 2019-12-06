12f02c196aa415685e70cd75b18e7d55433f342b
Meghan Stovall <meghan.stovall@gmail.com>
2019-12-05 20:00:50 -0700

    Updated the user test class and the user class

--- lib/user.rb
+++ lib/user.rb
@@ -10,4 +10,13 @@ class User
   def learn(joke)
     @jokes << joke
   end
+
+  def tell(other_user, joke)
+    other_user.jokes << joke
+  end
+
+  def joke_by_id(id_num)
+    index = id_num - 1
+    return @jokes[index]
+  end
 end
